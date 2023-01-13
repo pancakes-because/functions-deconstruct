4a215e6cb699db7510cd237dcf52ede30ac1630e
Diya O'Brien <112984845+pancakes-because@users.noreply.github.com>
2023-01-13 16:03:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+/* 
+
+    Pick something you like to do, or something that you can do well that involves many steps. 
+    Your job is to deconstruct your hobby, job, or craft into the tasks required.
+
+*/ 
+
+/* 
+
+    Task: Buy a cookie from Crumbl Cookies 
+
+    1. View cookie menu. 
+    2. Select a cookie to order. 
+    3. Choose store location. 
+    4. Drive to store location. 
+    5. Order desired cookie at check-out kiosk. 
+    6. Pay for cookie at check-out kiosk. 
+    7. Receive cookie order from staff. 
+    8. Leave store location. 
+    9. Eat cookie and enjoy! 
+
+*/ 
+
+/* 
+
+    Write a function for each task.
+    In the function body, just console.log() a description of the task.
+
+*/ 
+
+const viewCookieMenu = () => {
+    // Code to view the cookie menue, so I know what cookies are avaiable.
+    console.log("I view the cookie menue online, so I know what cookies are avaiable.") 
+}
+
+const selectCookieForOrder = () => {
+    // Code to select a cookie, so I know what I want at the store later. 
+    console.log("I selected a cookie, so I know what I want to order at the store later in person.") 
+}
+
+const chooseAStore = () => {
+    // Code to select the store I will go to to purchase my cookie.
+    console.log("I select the store I will go to to purchase my cookie.") 
+}
+
+const driveToStore = () => {
+    // Code for driving to the store I selected, so I can purchase my cookie. 
+    console.log("I drive to my chosen store location, so I can purchase my cookie.") 
+}
+
+const orderACookie = () => {
+     // Code for ordering desired cookie at check-out kiosk.
+     console.log("I order the cookie I want at the check-out kiosk.") 
+}
+
+const payForCookie = () => {
+    // Code to pay for cookie at check-out kiosk. 
+    console.log("I pay for the cookie that I ordered at the check-out kiosk.") 
+}
+
+const recieveCookie = () => {
+    // Code for recieving the cookie that the staff baked for me. 
+    console.log("I receive the cookie I payed for from the bakery staff.") 
+}
+
+const leaveStore = () => {
+    // Code for leaving the store after I received my cookie. 
+    console.log("I leave the store after receiving my cookie order.") 
+}
+
+const enjoyCookie = () => {
+    // Code for eating the cookie and enjoy it. 
+    console.log("I eat the cookie. It's really good!") 
+}
+
+/* 
+
+    Invoke the tasks in the correct order.
+
+*/ 
+
+viewCookieMenu()
+selectCookieForOrder()
+chooseAStore()
+driveToStore()
+orderACookie()
+payForCookie()
+recieveCookie()
+leaveStore()
+enjoyCookie()
+
